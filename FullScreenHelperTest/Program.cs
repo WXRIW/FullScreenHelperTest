77a27ea2a621a4f907a0720738f607fcef0f3ce5
XY Wang <wxy11787@foxmail.com>
2024-04-17 22:59:05 +0800

    Add project files.

---
+++ FullScreenHelperTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using Lyricify_for_Spotify.Helpers.Device;
+
+namespace FullScreenHelperTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserNotificationState? prevState = null;
+            while (true)
+            {
+                var state = FullscreenHelper.State();
+                if (prevState != state)
+                {
+                    prevState = state;
+                    Console.WriteLine(DateTime.Now.ToString("O") + " " + Enum.GetName(typeof(UserNotificationState), state));
+                }
+            }
+        }
+    }
+}
